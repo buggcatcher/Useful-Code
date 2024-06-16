8a27d2a342a9dbd5eac506337ea667547874ad7f
Bug Catcher <163436093+buggcatcher@users.noreply.github.com>
2024-06-16 22:51:46 +0200

    Update inherit2.kt

--- inherit2.kt
+++ inherit2.kt
@@ -1,3 +1,15 @@
+// In Kotlin, quando crei un oggetto di una classe come Captain che eredita da Pirate, i parametri vengono passati al costruttore principale della classe base (Pirate).
+// Questo costruttore principale è il primo a essere chiamato quando l'oggetto viene creato.
+// Il blocco init nella classe Pirate viene eseguito per primo.
+// Se la classe derivata Captain avesse anche un blocco init proprio, questo verrebbe eseguito subito dopo quello della classe base Pirate.
+
+// I parametri vengono passati al costruttore primario. In Kotlin, il blocco init viene chiamato quando l'oggetto viene creato.
+// Poiché Captain deriva dalla classe Pirate, cerca il blocco di inizializzazione nella classe base (Pirate) ed esegue questo.
+// Se Captain avesse un blocco init, il compilatore avrebbe eseguito anche il blocco init della classe derivata.
+// Successivamente, la funzione giveOrders() per l'oggetto pg1 viene chiamata usando l'istruzione pg1.giveOrders().
+// Il programma funziona in modo simile quando viene creato l'oggetto pg2 della classe SwordsMan. Esegue il blocco init della classe base.
+// Quindi, il metodo duel() della classe SwordsMan viene chiamato usando l'istruzione pg2.duel().
+
 open class Pirate(kills: Int, name: String) {
     init {
         println("My name is $name.")
@@ -18,9 +30,9 @@ class SwordsMan(kills: Int, name: String) : Pirate(kills, name) {
 }
 
 fun main(args: Array<String>) {
-    val t1 = Captain(35, "Blackbeard")
-    t1.giveOrders()
+    val pg1 = Captain(35, "Blackbeard")
+    pg1.giveOrders()
     println()
-    val f1 = SwordsMan(29, "Long John Silver")
-    f1.duel()
+    val pg2 = SwordsMan(29, "Long John Silver")
+    pg2.duel()
 }
