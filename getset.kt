ae0dbed54bad91a14c8fff40373cf87de029c50e
Bug Catcher <163436093+buggcatcher@users.noreply.github.com>
2024-06-16 21:12:24 +0200

    Update getset.kt

--- getset.kt
+++ getset.kt
@@ -2,12 +2,16 @@
 fun main() {
     val maria = Girl()
     maria.age = 15
+    maria.profession = "studentessa"
     val giulia = Girl()
     giulia.age = 35
-    println("Maria dice di avere ${maria.age} anni. Giulia dice di averne ${giulia.age}.")
+    giulia.profession = "dottoressa"
+    println("Maria e' una ${maria.profession} e dice di avere ${maria.age} anni.\nGiulia e' una ${giulia.profession} e dice di averne ${giulia.age}.")
 }
  
 class Girl {
+    // non serve inizializzare subito, lo puoi fare dopo con lateinit
+    lateinit var profession: String
     var age: Int = 0
     get() = field
     set(value) {
