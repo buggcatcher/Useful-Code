a1785ece3e94806bf1c8cd66862507c5673ee01e
Bug Catcher <163436093+buggcatcher@users.noreply.github.com>
2024-06-16 19:50:03 +0200

    Create getter&setter.kt

---
+++ getter&setter.kt
@@ -0,0 +1,36 @@
+// getters & setters sono generati automaticamente anche se non esplicitati
+fun main() {
+    val maria = Girl()
+    maria.age = 15
+    val giulia = Girl()
+    giulia.age = 35
+    println("Maria dice di avere ${maria.age} anni. Giulia dice di averne ${giulia.age}.")
+}
+
+class Girl {
+    var age: Int = 0
+    get() = field
+    set(value) {
+        field = if (value < 18)
+        	18
+        else if (value >= 18 && value <= 30)
+        	value
+        else
+        	value - 5
+    }
+}
+
+// class Casa {
+//     var indirizzo: String = "Piazza degli Antinori"
+// }
+//
+// equivale a scrivere:
+//
+// class Casa {
+//     var indirizzo: String = "Piazza degli Antinori"
+    
+//     get() = field
+//     set(value) {
+//         field = value
+//     }
+// }
